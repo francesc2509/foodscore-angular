505e93a6d10a12588a4e251343ab9b6802b77b8d
Francesc Manarel Bueno <fcml@gft.com>
2018-11-05 14:40:36 +0100

    Added restaurant card list to RestaurantsPageComponent template

--- src/app/components/restaurants-page/restaurants-page.component.ts
+++ src/app/components/restaurants-page/restaurants-page.component.ts
@@ -9,20 +9,46 @@ import { Restaurant } from '../../models';
 })
 export class RestaurantsPageComponent implements OnInit {
     readonly days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    daysOpen: boolean[] = new Array(7).fill(true);
-    weekDay: number;
+    readonly weekDay = new Date().getDay();
+    daysOpen: boolean[];
 
     restaurants: Restaurant[] = [];
-    newRest: Restaurant = {
-        name: '',
-        description: '',
-        daysOpen: [],
-        cuisine: '',
-        image: '',
-        phone: ''
-    };
+    newRest: Restaurant;
 
     ngOnInit(): void {
+        this.resetForm();
+    }
 
+    addRestaurant(): void {
+        this.newRest.daysOpen = this.daysOpen.reduce((days, isSelected, i) => isSelected ? [...days, i] : days, []);
+        this.restaurants.push(this.newRest);
+        this.resetForm();
+    }
+
+    changeImage(fileInput: HTMLInputElement): void {
+        if (!fileInput.files || fileInput.files.length === 0) {
+            return;
+        }
+        const reader: FileReader = new FileReader();
+        reader.readAsDataURL(fileInput.files[0]);
+        reader.addEventListener('loadend', e => {
+            this.newRest.image = reader.result.toString();
+        });
+    }
+
+    resetForm(): void {
+        this.newRest = {
+            name: '',
+            description: '',
+            daysOpen: [],
+            cuisine: '',
+            image: '',
+            phone: ''
+        };
+        this.daysOpen = new Array(7).fill(true);
+    }
+
+    getDaysString(restaurant: Restaurant): string {
+        return restaurant.daysOpen.map(day => this.days[day]).join(', ');
     }
 }
